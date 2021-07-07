f6a63cb9bd08247a29bdf1636aab34fd9f1734d6
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 16:24:52 +0530

    Create userinput.c

---
+++ userinput.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int var1;
+	//Take input and store it in var1 using scanf
+	//Print the variable var1 using printf
+    scanf("%d",&var1);
+    printf("%d",var1);
+
+
+return 0;
+}
