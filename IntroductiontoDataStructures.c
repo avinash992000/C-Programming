0ddf42d531e6e3b7785a2cc3a58e32ae8315d8ac
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 20:14:17 +0530

    Create IntroductiontoDataStructures.c

---
+++ IntroductiontoDataStructures.c
@@ -0,0 +1,16 @@
+Data Structures:
+    1. Def: Organizing data in such a manner is known as data Structures.
+    2. Classification of data Structures:
+        1. Linear: Organizing in linear / sequential manner 
+            i. Arrays:
+                -> Advantages:
+                    -> Faster execution
+                    -> Random Access using Index value
+                -> Disadvantages:
+                    -> Fixed - size
+            ii. Linked List:
+                -> Advantages:
+                    -> Grown in size
+            iii. Stacks
+            iv. Queues
+        2. Non - Linear: 
