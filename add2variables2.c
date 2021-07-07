a297be062e882a7ed67ddeced0e547a606ba52cc
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 16:18:28 +0530

    Create add2variables2.c

---
+++ add2variables2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int var1 = 100;
+	int var2 = 150;
+	//Declare a variable answer here and store in it
+	//The sum of var1 and var2.
+
+	printf("%d",var1+var2);
+	return 0;
+}
