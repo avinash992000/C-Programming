b61081a4829d6e460199824e5b6d48787f55d434
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 19:56:08 +0530

    Create inputastring.c

---
+++ inputastring.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[50];
+	//input string here using scanf
+    scanf("%s",str);
+
+	//print string here using printf
+	printf("%s",str);
+	return 0;
+}
