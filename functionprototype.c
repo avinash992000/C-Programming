823c2a7578acab62b748e51cec55051987880995
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 20:03:45 +0530

    Create functionprototype.c

---
+++ functionprototype.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//add function prototype here
+int add(int var1, int var2);
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	int sum = add(a, b);
+	printf("%d", sum);
+	return 0;
+}
+int add(int var1, int var2)
+{
+	return (var1 + var2);
+}
