7da837fb911ea5f03aff9b24b142ce0cd3b0910b
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 19:52:14 +0530

    Create resizinganarray.c

---
+++ resizinganarray.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int* arr = (int*) malloc(sizeof(int) * 3);
+	int i, N;
+    scanf("%d",&N);
+	//input N and then use realloc to change size of array arr.
+    realloc(arr,sizeof(int)*10);
+    for(i=0;i<N;i++){
+        scanf("%d",arr+i);
+    }
+	for(i=N-1;i>=0;i--){
+        printf("%d ",*arr+i);
+    }
+	return 0;
+}
