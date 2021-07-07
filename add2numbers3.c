3d067bfcc19a82b6892c1f299e16c4361a756b53
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 16:26:48 +0530

    Create add2numbers3.c

---
+++ add2numbers3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int var1, var2, sum;
+	
+	//Use scanf to input var1 and var2
+	//Store their sum in variable sum
+	//Use printf to print the value of sum
+    scanf("%d %d",&var1,&var2);
+    sum=var1+var2;
+    printf("%d",sum);
+
+
+
+	return 0;
+}
