13100cf180e357f2f178bb37349feb362a8b716b
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 16:01:50 +0530

    Create declareavariable.c

---
+++ declareavariable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	//Declare the variable num here
+    int num;
+    num = 5;
+	//Assign value of 5 to the variable num here
+
+
+	printf("%d", num);//Note that %d is used print an integer and after a
+	return 0;		//comma, the integer to be printed.
+}
