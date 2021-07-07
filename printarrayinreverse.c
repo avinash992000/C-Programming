b303fb8d36cf7b0689ec7c9f93be5f327ee5d9a4
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 16:54:27 +0530

    Create printarrayinreverse.c

---
+++ printarrayinreverse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i, arr[5];
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+    for(i=4;i>=0;i--){
+        printf("%d ",arr[i]);
+    }
+	//write the code here to print array in reverse
+
+
+	return 0;
+}
