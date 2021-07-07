26b5e41105bbf5d5490dba6dd31459fdb4e3b308
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 16:31:22 +0530

    Create ifelseif.c

---
+++ ifelseif.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int var1, var2;
+    scanf("%d %d",&var1,&var2);
+    if(var1>var2){
+        printf("value1 is greater");}
+    else if(var1==var2){
+        printf("both values are equal");}
+    else{
+        printf("value2 is greater");}
+    
+	return 0;
+}
