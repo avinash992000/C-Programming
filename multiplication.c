b6bb43f300cb6dcbcc3db5e4b6307ad8871b0afb
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 16:48:58 +0530

    Create multiplication.c

---
+++ multiplication.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int i, N;
+	scanf("%d", &N);
+	//write a loop that runs for 10 iterations
+    for(i=1;i<=10;i++){
+        printf("%d ",N*i);
+    }
+	
+	return 0;
+}
