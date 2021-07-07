228ea7ce30bd2d6bfbf66a6b585da033d17298fb
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 16:22:19 +0530

    Create mathorlogic.c

---
+++ mathorlogic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    unsigned int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    printf("%u",a+b+c);
+    return 0;
+}
