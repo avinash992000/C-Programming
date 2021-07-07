2874e0e451e85e8733e42d478fe61ee7fb726219
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 20:46:59 +0530

    Create enumtest.c

---
+++ enumtest.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+enum point{x,y};
+enum point1{x,y};
+//NOte: Same names of enumerators will not be given for multiple enums in single program
+
+int main()
+{
+    return 0;
+}
