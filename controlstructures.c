af378c50e2f2350d8234b739544729eab2c2a903
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 20:12:24 +0530

    Create controlstructures.c

---
+++ controlstructures.c
@@ -0,0 +1,8 @@
+/*
+    Statements:
+        -> Sequential statements: Execution will takes place statement by statement [Order]
+    => Control Structures: COntrolling the flow of execution based on a condition
+        -> COnditional statements: if[simple if, if else, else if ladder, nested if] and switch
+        -> Iterative / Looping Statements: while, for and do .. while loop 
+        -> Jumping STatements: break, continue, goto and return[Used in functions]
+*/
