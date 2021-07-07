fb5a11dcf7f0e39d8b60e6e870e108c53b2b26f1
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-07 20:45:54 +0530

    Create elseifladdertest.c

---
+++ elseifladdertest.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int x = 100;
+    if(x == 100)        printf("Equal to 100");
+    else if(x == 100)   printf("Equal to 100");
+    else if(x == 100)   printf("Equal to 100");
+    else                printf("Not Equal to 100");
+}
