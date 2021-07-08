fd21d9ba948bf385abe854c84497f34bd376c5f2
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-08 11:53:22 +0530

    Create ifelse.c

---
+++ ifelse.c
@@ -0,0 +1,17 @@
+/*
+    if - else statement:
+        -> Syntax:
+            if(expression / constant[Zero/Non-Zero] / variable)
+            {
+                -statements1
+            }
+            else
+            {
+                -statements2
+            }
+        -> Execution:
+            -> if expression value is true/non-zero statements1 will be executed
+            -> if expression value is false/zero statements2 will be executed
+            -> Note: Only one statement will be executed [Either stmt1 or stmt2]
+        -> if - else can be called as two-way branching statement
+*/
