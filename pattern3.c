92ee6f2f5f3b14f908cba8fd0efda5f4a903edef
avinash992000 <86355693+avinash992000@users.noreply.github.com>
2021-07-08 12:33:16 +0530

    Create pattern3.c

---
+++ pattern3.c
@@ -0,0 +1,24 @@
+/*
+    number of rows = 5
+    *****
+    ****
+    ***
+    **
+    *
+*/
+#include<stdio.h>
+int main()
+{
+    int numberOfRows;
+    printf("How many rows you want to print? Answer: ");
+    scanf("%d",&numberOfRows);
+
+    int i,j;
+    for(i=numberOfRows;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
