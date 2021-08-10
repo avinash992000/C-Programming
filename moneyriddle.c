76ea2497c10d075b4dfc5e6a97f4970e35f8d7b9
AVINASH <86355693+avinash992000@users.noreply.github.com>
2021-08-10 19:57:20 +0530

    Create moneyriddle.c

---
+++ moneyriddle.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+int main(){
+    
+    int n,count=0,count1=0;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    int m=(n-1);
+    int p=0;
+    int x=1;
+    while(m>p){
+        if(x%2!=0){
+            if(a[p]>a[m]){
+            count+=a[p];
+            //printf("%d\n",a[p]);
+            p++;x++;
+            }
+            else{
+                count+=a[m];
+                //printf("%d\n",a[m]);
+                m--;x++;
+            }
+        }
+        else{
+            if(a[p]>a[m]){
+                p++;x++;}
+            else{
+                m--;x++;}
+        }
+        
+    }
+    printf("%d",count);
+    
+    return 0;
+}
